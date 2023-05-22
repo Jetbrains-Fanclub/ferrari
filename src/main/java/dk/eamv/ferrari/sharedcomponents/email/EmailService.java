648507bedc0e26d74a88c97c586758092f80da70
Pviksy <birkegaardm@gmail.com>
2023-05-22 12:00:51 +0200

    Started implementation of email with SimpleJavaMail

---
+++ src/main/java/dk/eamv/ferrari/sharedcomponents/email/EmailService.java
@@ -0,0 +1,33 @@
+package dk.eamv.ferrari.sharedcomponents.email;
+
+import org.simplejavamail.api.email.Email;
+import org.simplejavamail.api.mailer.config.TransportStrategy;
+import org.simplejavamail.email.EmailBuilder;
+import org.simplejavamail.mailer.MailerBuilder;
+
+public class EmailService {
+
+    public static void sendEmail() {
+        Email email = EmailBuilder.startingBlank()
+                .from("Sælgeren", "EMAIL_ADDRESS_HERE")
+                .to("Salgschefen", "TO_EMAIL_ADDRESS_HERE")
+                .withSubject("Lån overskrider min grænse")
+                .withPlainText("""
+                        Hej salgschef
+                        
+                        Jeg har modtaget oprette et lånetilbud som overskrider min tilladte beløbsgrænse.
+                        Vil du godkende aftalen?
+                        
+                        Mvh
+                        Sælger
+                        """)
+                .buildEmail();
+
+
+        new Thread(() -> MailerBuilder
+                .withSMTPServer("Smtp.gmail.com", 587, "EMAIL_ADDRESS_HERE", "EMAIL_PASSWORD_HERE")
+                .withTransportStrategy(TransportStrategy.SMTP_TLS)
+                .buildMailer()
+                .sendMail(email)).start();
+    }
+}
